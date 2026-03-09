6da36b07fc942853059fbeea8c3c0dc935465656
lingqiancariad <lingqian.meng@cariad.technology>
2026-03-09 01:01:25 +0100

    real first commit

---
+++ book_class.cpp
@@ -0,0 +1,77 @@
+/*
+Update your Book class to support:
+
+    A default constructor that initializes the book with empty/default values
+
+    A parameterized constructor that sets the book’s title, author, and page count
+
+    A destructor that prints a message when a Book object is destroyed "Destorying book : <title>"
+
+Then update your main() function to create books using both constructors.
+*/
+#include <iostream>
+#include <string>
+
+class Book {
+private:
+    std::string title;
+    std::string author;
+    int pageCount;
+
+public:
+    // TODO: Add a default constructor
+    Book () {
+        title = "unname";
+        author = "unknown";
+        pageCount = 0;
+    }
+    // TODO: Add a parameterized constructor
+    Book (std::string t, std::string a, int p) {
+        title = t;
+        author = a;
+        pageCount = p;
+    }
+    // TODO: Add a destructor
+    ~Book(){
+        std::cout << "Book object now destructed.\n";
+    }
+
+    // Getters
+    std::string getTitle() { 
+        return title; 
+    }
+    std::string getAuthor() { 
+        return author; 
+    }
+    int getPageCount() { 
+        return pageCount; 
+    }
+};
+
+int main() {
+    //TODO: Use the default constructor to initialize a defaultBook
+    Book defaultBook;
+
+    //TODO: Use the parametrized constructor to initialize b1 and b2
+    Book b1("The falling of big to fall", "Marin S", 499);
+    Book b2("The arising of nothing", "Stefan S", 501);
+    Book* b1_p = &b1;
+    Book* b2_p = &b2;
+    
+    std::cout << "Default Book:\n";
+    std::cout << "Title: " << defaultBook.getTitle() << "\n";
+    std::cout << "Author: " << defaultBook.getAuthor() << "\n";
+    std::cout << "Pages: " << defaultBook.getPageCount() << "\n\n";
+
+    std::cout << "Book 1:\n";
+    std::cout << "Title: " << (*b1_p).getTitle() << "\n";
+    std::cout << "Author: " << (*b1_p).getAuthor() << "\n";
+    std::cout << "Pages: " << (*b1_p).getPageCount() << "\n\n";
+
+    std::cout << "Book 2:\n";
+    std::cout << "Title: " << b2_p->getTitle() << "\n";
+    std::cout << "Author: " << b2_p->getAuthor() << "\n";
+    std::cout << "Pages: " << b2_p->getPageCount() << "\n\n";
+
+    return 0;
+}
